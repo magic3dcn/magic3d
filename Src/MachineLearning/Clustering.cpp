842e13d8164cd38df8160d6a33aee6652fbbcc06
magic3dcn <magic3d@163.com>
2014-04-02 08:45:46 +0800

    update.

--- Src/MachineLearning/Clustering.cpp
+++ Src/MachineLearning/Clustering.cpp
@@ -1,6 +1,7 @@
 #include "Clustering.h"
 #include "../Common/LogSystem.h"
 #include "Eigen/Eigenvalues"
+#include "flann/flann.h"
 
 namespace MagicML
 {
@@ -171,6 +172,5 @@ namespace MagicML
     void Clustering::MeanshiftValue(const std::vector<double>& sourceData, int dim, double h, 
                                     const std::vector<double>& inputData, std::vector<double>& resData)
     {
-
     }
 }
