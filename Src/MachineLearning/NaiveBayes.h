66055bf89c21035a94b05626b2df75b141f2b926
liguo <liguo.magic3d@gmail.com>
2014-06-05 22:16:49 +0800

    Update MagicLib.

--- Src/MachineLearning/NaiveBayes.h
+++ Src/MachineLearning/NaiveBayes.h
@@ -3,13 +3,18 @@
 
 namespace MagicML
 {
+    class GaussianModel;
     class NaiveBayes
     {
     public:
         NaiveBayes();
         ~NaiveBayes();
+        
+        void Learn(const std::vector<double>& dataX, const std::vector<int>& dataY);
+        int Predict(const std::vector<double>& dataX);
 
     private:
-
+        std::vector<double> mCategoryProList;
+        std::vector<GaussianModel*> mGaussianList;
     };
 }
